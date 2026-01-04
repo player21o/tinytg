4c2bbdc92c9eeff8f112f6ba290eed34d72b7925
player210 <196048914+player21o@users.noreply.github.com>
2026-01-05 00:16:21 +0100

    feat: implemented methods

--- methods.ts
+++ methods.ts
@@ -1,9 +1,39 @@
 import type { Methods } from "./types";
 
+declare global {
+  interface Window {
+    TelegramWebviewProxy?: {
+      postEvent?: (event: string, data: string) => any;
+    };
+    external?: {
+      notify: (event_data: string) => any;
+    };
+  }
+}
+
 class TelegramMethodSender {
   constructor() {}
 
-  private _post<T extends keyof Methods>(method: T, data: Methods[T]) {}
+  public post<T extends keyof Methods>(method: T, data: Methods[T]["params"]) {
+    if (
+      window["TelegramWebviewProxy"] != undefined &&
+      window.TelegramWebviewProxy.postEvent != undefined
+    ) {
+      window.TelegramWebviewProxy.postEvent(method, JSON.stringify(data));
+    } else if (
+      window["external"] != undefined &&
+      window["external"].notify != undefined
+    ) {
+      window["external"].notify(
+        JSON.stringify({ eventType: method, eventData: data })
+      );
+    } else {
+      window.parent.postMessage(
+        JSON.stringify({ eventType: method, eventData: data }),
+        "https://web.telegram.org"
+      );
+    }
+  }
 }
 
 export const methods = new TelegramMethodSender();
